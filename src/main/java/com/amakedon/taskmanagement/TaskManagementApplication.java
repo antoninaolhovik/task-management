fc42fc05cd1683a1d78625747f3cb485d17d1c69
aolhovik <toshaolhovik@gmail.com>
2020-04-01 20:29:39 +0300

    Actuators added.

--- src/main/java/com/amakedon/taskmanagement/TaskManagementApplication.java
+++ src/main/java/com/amakedon/taskmanagement/TaskManagementApplication.java
@@ -1,11 +1,7 @@
 package com.amakedon.taskmanagement;
 
-import com.amakedon.taskmanagement.persistence.model.Project;
-import com.amakedon.taskmanagement.service.ProjectService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -16,16 +12,12 @@ public class TaskManagementApplication {
 
 	private static final Logger LOG = LoggerFactory.getLogger(TaskManagementApplication.class);
 
-	@Value("${additional.info}")
-	private String additional;
-
 	public static void main(String[] args) {
 		SpringApplication.run(TaskManagementApplication.class, args);
 	}
 
 	@PostConstruct
 	private void postConstruct() {
-		LOG.info("Additional Property {}", additional);
 	}
 
 }
--- src/main/java/com/amakedon/taskmanagement/config/PersistenceConfig.java
+++ src/main/java/com/amakedon/taskmanagement/config/PersistenceConfig.java
@@ -1,9 +1,7 @@
 package com.amakedon.taskmanagement.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
-@PropertySource("classpath:additional.properties")
 @Configuration
 public class PersistenceConfig {
 
